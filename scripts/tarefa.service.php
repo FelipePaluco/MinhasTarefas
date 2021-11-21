6659efc8fb541cf483597383045850304e1bc78a
Felipe Paluco <87909998+FelipePaluco@users.noreply.github.com>
2021-11-21 15:50:24 -0300

    Implantado filtro por ID de Usuário

--- scripts/tarefa.service.php
+++ scripts/tarefa.service.php
@@ -1,6 +1,8 @@
 <?php
-
-
+session_start();
+if(!isset($_SESSION['logado']) || $_SESSION['logado'] == false) {
+	Header('Location: http://localhost/app_tarefa/www/index.php');
+}
 //CRUD
 class TarefaService {
 
@@ -13,29 +15,30 @@ class TarefaService {
 	}
 
 	public function inserir() { //Create
-		$query = 'insert into tb_tarefas(tarefa)values(?)';
+		$query = 'insert into tb_tarefas(id_usuario, tarefa)values(?, ?)';
 		$stmt = $this->conexao->prepare($query);
-		$stmt->bindValue(1, $this->tarefa->__get('tarefa'));
+		$stmt->bindValue(1, $this->tarefa->__get('id_usuario'));
+		$stmt->bindValue(2, $this->tarefa->__get('tarefa'));
 		$stmt->execute();
 	}
 
 	public function recuperar($tipo) { //Read
 		
 		if($tipo == 'todos') {
-			$query = ' select t.id, s.status, t.tarefa  from tb_tarefas as t left join tb_status as s on (t.id_status = s.id)';
-
+			$query = 'select t.id_usuario, t.id, s.status, t.tarefa from tb_tarefas as t left join tb_status as s on (t.id_status = s.id) WHERE t.id_usuario = '.$_SESSION['idUsuario'];
 			$stmt = $this->conexao->prepare($query);
 			$stmt->execute();
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 
+
 		} else if($tipo == 'pendentes') {
-			$query = 'select * from tb_tarefas where id_status = 1';
+			$query = 'select * from tb_tarefas where id_status = 1 and id_usuario = '.$_SESSION['idUsuario'];
 			$stmt = $this->conexao->prepare($query);
 			$stmt->execute();
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		}
 		else if($tipo == 'realizadas') {
-			$query = 'select * from tb_tarefas where id_status = 2';
+			$query = 'select * from tb_tarefas where id_status = 2 and id_usuario = '.$_SESSION['idUsuario'];
 			$stmt = $this->conexao->prepare($query);
 			$stmt->execute();
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -53,6 +56,14 @@ class TarefaService {
 
 	
 	public function marcarAtualizada() { //Update
+		$query = 'update tb_tarefas set id_status = ? where id = ?';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(1, $this->tarefa->__get('id_status'));
+		$stmt->bindValue(2, $this->tarefa->__get('id'));
+		return $stmt->execute();
+	}
+
+	public function marcarPendente() { //Update
 		$query = 'update tb_tarefas set id_status = ? where id = ?';
 		$stmt = $this->conexao->prepare($query);
 		$stmt->bindValue(1, $this->tarefa->__get('id_status'));
