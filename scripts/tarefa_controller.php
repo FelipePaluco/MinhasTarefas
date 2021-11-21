1437ff214d8bf6d279a0fc51a6d2280423a504a6
Felipe Paluco <87909998+FelipePaluco@users.noreply.github.com>
2021-11-21 15:50:58 -0300

    Lidando com sistema de login e tarefas pendentes..

--- scripts/tarefa_controller.php
+++ scripts/tarefa_controller.php
@@ -3,6 +3,7 @@
     require "conexao.php";
     require "tarefa.service.php";
     require "tarefa.model.php";
+    
 
     $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
@@ -13,6 +14,8 @@
         } else {       
             $tarefa = new Tarefa();
             $tarefa->__set('tarefa', $_POST['tarefa']);
+            $tarefa->__set('id_usuario', $_SESSION['idUsuario']);
+
 
             $conexao = new Conexao();
             $tarefaService = new TarefaService($conexao, $tarefa);
@@ -46,18 +49,14 @@
         $conexao = new Conexao();
         $tarefaService = new TarefaService($conexao, $tarefa);
         if($tarefaService->atualizar()) {
-            if(isset($_GET['pag']) && $_GET['pag'] == 'index') {
-                Header('Location: http://localhost/app_tarefa/www/index.php?editado=true');
-            } else {
+            if(isset($_GET['pag']) && $_GET['pag'] == 'dashboard') {
+                Header('Location: http://localhost/app_tarefa/www/dashboard.php?editado=true');
+            } else if(isset($_GET['pag']) && $_GET['pag'] == 'tarefas_pendentes') {
                 Header('Location: http://localhost/app_tarefa/www/tarefas_pendentes.php?editado=true');
-            }
-        }  else {
-            if(isset($_GET['pag']) && $_GET['pag'] == 'index') {
-                Header('Location: http://localhost/app_tarefa/www/index.php?editado=false');
-            } else {
-                Header('Location: http://localhost/app_tarefa/www/tarefas_pendentes.php?editado=false');
-        }
-    }
+            } else if(isset($_GET['pag']) && $_GET['pag'] == 'tarefas_realizadas') {
+                Header('Location: http://localhost/app_tarefa/www/tarefas_realizadas.php?editado=true');
+            } 
+        } 
 
     } else if($acao == 'remover') {
         $tarefa = new Tarefa();
@@ -66,8 +65,8 @@
         $conexao = new Conexao();
         $tarefaService = new TarefaService($conexao, $tarefa);
         $tarefaService->remover();
-        if(isset($_GET['pag']) && $_GET['pag'] == 'index') {
-            Header('Location: http://localhost/app_tarefa/www/index.php?removido=true');
+        if(isset($_GET['pag']) && $_GET['pag'] == 'dashboard') {
+            Header('Location: http://localhost/app_tarefa/www/dashboard.php?removido=true');
         } else {
             Header('Location: http://localhost/app_tarefa/www/tarefas_pendentes.php?removido=true');
         }
@@ -82,13 +81,30 @@
 
         $tarefaService = new TarefaService($conexao, $tarefa);
         $tarefaService->marcarAtualizada();
-        if(isset($_GET['pag']) && $_GET['pag'] == 'index') {
-            Header('Location: http://localhost/app_tarefa/www/index.php');
+        if(isset($_GET['pag']) && $_GET['pag'] == 'dashboard') {
+            Header('Location: http://localhost/app_tarefa/www/dashboard.php');
         } else {
             Header('Location: http://localhost/app_tarefa/www/tarefas_pendentes.php');
         }
+    }
+    else if($acao == 'marcarPendente') {
+        $tarefa = new Tarefa();
 
+        $tarefa->__set('id', $_GET['id']);
+        $tarefa->__set('id_status', 1);
 
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->marcarPendente();
+        if(isset($_GET['pag']) && $_GET['pag'] == 'dashboard') {
+            Header('Location: http://localhost/app_tarefa/www/dashboard.php');
+        } else if(isset($_GET['pag']) && $_GET['pag'] == 'tarefas_pendentes') {
+            Header('Location: http://localhost/app_tarefa/www/tarefas_pendentes.php');
+        }
+        else if(isset($_GET['pag']) && $_GET['pag'] == 'tarefas_realizadas') {
+            Header('Location: http://localhost/app_tarefa/www/tarefas_realizadas.php');
+        }
     }
     
 
